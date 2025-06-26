6b809c7e81e4723bab27a92e1e86c3f3eb35d45a
Kittiphat S <kittiphat@muze.co.th>
2025-06-26 19:32:10 +0700

    chore: update preload behavior

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -91,18 +91,21 @@
         <nav class="hidden md:ml-8 md:flex md:space-x-8">
           <a
             href="/"
+            data-sveltekit-preload-data="hover"
             class="px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
           >
             Dashboard
           </a>
           <a
             href="/workflows"
+            data-sveltekit-preload-data="hover"
             class="px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
           >
             Workflows
           </a>
           <a
             href="/projects"
+            data-sveltekit-preload-data="hover"
             class="px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
           >
             Projects
